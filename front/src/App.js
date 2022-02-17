ccf10a79c39960028cd732d00c68a826359dd4be
Kevin <kevin_vergara.rodriguez@yahoo.es>
2022-02-16 22:50:15 -0500

    Listado de blogs

--- front/src/App.js
+++ front/src/App.js
@@ -1,24 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 
+import CompShowBlog from './blog/ShowBlog';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <button className='btn btn-primary'><i className='fab fa-react'></i></button>
       </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={ <CompShowBlog/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
